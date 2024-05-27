fe7855e8c691af7973ec7b9dd477fdc46a67c3e3
Mikhail <michail.77@mail.ru>
2024-05-27 12:53:29 +0300

    1

--- 01_terraform/provider.tf
+++ 01_terraform/provider.tf
@@ -6,6 +6,16 @@ terraform {
     }
   }
   required_version = ">=0.13"
+    backend "s3" {
+    # Укажите данные вашего S3 bucket для хранения состояния Terraform
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "yc-backend"
+    region   = "ru-central1"
+    key      = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 
